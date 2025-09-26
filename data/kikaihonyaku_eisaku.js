d9c2c36a0a1444d0ff6ecb0f78950416d5c949fe
kapihaya <kapihaya8810@gmail.com>
2025-09-26 15:03:42 +0900

    Add files via upload

---
+++ data/kikaihonyaku_eisaku.js
@@ -0,0 +1,64 @@
+var kikaiHonyakuEsakuData = {
+    "第1回": `
+Question,Answer
+
+`,
+    "第2回": `
+Question,Answer
+
+`,
+    "第3回": `
+Question,Answer
+
+`,
+    "第4回": `
+Question,Answer
+
+`,
+    "第5回": `
+Question,Answer
+
+`,
+    "第6回": `
+Question,Answer
+
+`,
+    "第7回": `
+Question,Answer
+
+`,
+    "第8回": `
+Question,Answer
+
+`,
+    "第9回": `
+Question,Answer
+
+`,
+    "第10回": `
+Question,Answer
+
+`,
+    "第11回": `
+Question,Answer
+
+`,
+    "第12回": `
+Question,Answer
+
+`,
+    "第13回": `
+Question,Answer
+
+`,
+    "第14回": `
+Question,Answer
+
+`,
+    "第15回": `
+Question,Answer
+
+`,
+
+    // ... 他の単元も同様に追加 ...
+};
